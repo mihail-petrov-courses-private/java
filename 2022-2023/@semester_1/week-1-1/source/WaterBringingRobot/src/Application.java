a95db0a855de3b1f9ba4c784c615025613122ca0
Mihail Petrov <mail@mihail-petrov.me>
2022-03-10 21:49:14 +0200

    week-1-2: Introduction to Java

---
+++ 2022-2023/@semester_1/week-1-1/source/WaterBringingRobot/src/Application.java
@@ -0,0 +1,9 @@
+class Application {
+
+    public static void main(String[] args) {
+
+        // Това е коментар:
+        // Сега искам да инструктирам компютъра да ми донесе вода
+        System.out.println("Моля донеси ми вода");
+    }
+}
